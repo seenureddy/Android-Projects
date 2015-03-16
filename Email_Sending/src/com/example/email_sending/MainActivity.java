4561c03f344a904b1530f34ec46c0a4a8df02914
seenureddy <srinivasulureddy.sura30@gmail.com>
2015-03-16 17:30:06 +0530

    Changed the dynamic way of sending an email

--- Email_Sending/src/com/example/email_sending/MainActivity.java
+++ Email_Sending/src/com/example/email_sending/MainActivity.java
@@ -7,14 +7,23 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends Activity {
-
+    
+	private EditText recipient;
+	private EditText subject;
+	private EditText body;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		recipient = (EditText) findViewById(R.id.recipient);
+		subject = (EditText) findViewById(R.id.subject);
+		body = (EditText) findViewById(R.id.body);
+		
 		Button email_send_button = (Button) findViewById(R.id.sendingEmail);
 		email_send_button.setOnClickListener(new View.OnClickListener() {
 			
@@ -22,6 +31,9 @@ public class MainActivity extends Activity {
 			public void onClick(View v) {
 				// TODO Auto-generated method stub
 				sendingEmail();
+				recipient.setText("");
+				subject.setText("");
+				body.setText("");
 				
 			}
 		});
@@ -30,19 +42,18 @@ public class MainActivity extends Activity {
 	protected void sendingEmail() {
 		// TODO Auto-generated method stub
 		Log.i("sending Email", "");
-		String [] To = {"seenu@agiliq.com"};
-		String [] Cc = {"shiva@agiliq.com"};
+		String [] recipients = {recipient.getText().toString()};
 		Intent emailIntent = new Intent(Intent.ACTION_SEND, Uri.parse("malito:"));
 		//emailIntent.setData(Uri.parse("malito"));
 		emailIntent.setType("text/plain");
 		
-		emailIntent.putExtra(Intent.EXTRA_EMAIL, To);
-		emailIntent.putExtra(Intent.EXTRA_CC, Cc);
-		emailIntent.putExtra(Intent.EXTRA_SUBJECT, "Email Sending test");
-		emailIntent.putExtra(Intent.EXTRA_TEXT, "are you received the email");
+		emailIntent.putExtra(Intent.EXTRA_EMAIL, recipients);
+		//emailIntent.putExtra(Intent.EXTRA_CC, Cc);
+		emailIntent.putExtra(Intent.EXTRA_SUBJECT, subject.getText().toString());
+		emailIntent.putExtra(Intent.EXTRA_TEXT, body.getText().toString());
 		
 		try {
-			startActivity(Intent.createChooser(emailIntent, "Sending Email"));
+			startActivity(Intent.createChooser(emailIntent, "Choose an email client from ... "));
 			finish();
 			Log.i("Finished the sending email", "");
 		}catch(android.content.ActivityNotFoundException ex) {
